70b658ef3b7c02320c3fc0b11bb2e3086566d45d
AxeKam333 <olek.kaminski5@gmail.com>
2024-12-16 23:16:01 +0100

    test: add mimic btns function. add naming convention

--- frontend/words-client/mainwindow.cpp
+++ frontend/words-client/mainwindow.cpp
@@ -6,10 +6,40 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // here add connect ui obj to fun. example:
+    // connect(ui->conectBtn, &QPushButton::clicked, this, &MyWidget::connectBtnHit);
+
+    connect(ui->connectButton, &QPushButton::clicked, this, &MainWindow::connectBtnHit);
+    connect(ui->addressLineEdit, &QLineEdit::returnPressed, this, &MainWindow::connectBtnHit);
+
+    connect(ui->acceptButton, &QPushButton::clicked, this, &MainWindow::sendBtnHit);
+    connect(ui->usernameLineEdit, &QLineEdit::returnPressed, this, &MainWindow::sendBtnHit);
+
+    connect(ui->disconnectButton, &QPushButton::clicked, this, &MainWindow::disconnectBtnHit);
 }
 
 MainWindow::~MainWindow()
 {
+    // if(sock)
+    //    sock->close();
     delete ui;
 }
 
+void MainWindow::connectBtnHit(){
+    ui->plainTextEdit->appendPlainText("connecting...");
+
+    ui->plainTextEdit->appendPlainText("connected successfully");
+}
+
+void MainWindow::sendBtnHit(){
+    ui->plainTextEdit->appendPlainText("waiting for server response...");
+
+    ui->plainTextEdit->appendPlainText("username accepted");
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::disconnectBtnHit(){
+    ui->plainTextEdit->appendPlainText("disconnected");
+    ui->stackedWidget->setCurrentIndex(0);
+}
--- frontend/words-client/mainwindow.h
+++ frontend/words-client/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPlainTextEdit>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QSpinBox>
+#include <QLCDNumber>
+#include <QStackedWidget>
+#include <QStatusBar>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +24,31 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    // 1 strona
+    QLineEdit *addressLineEdit;
+    QSpinBox *portSpinBox;
+    QPushButton *connectButton;
+    QPlainTextEdit *plainTextEdit;
+    QLineEdit *usernameLineEdit;
+    QPushButton *acceptButton;
+
+    // 2 strona
+    QLineEdit *lettersLineEdit;
+    QLCDNumber *lcdNumber;
+    QPushButton *disconnectButton;
+    QPlainTextEdit *plainTextEdit_2;
+    QPlainTextEdit *statsPlainTextEdit;
+    QLineEdit *wordLineEdit;
+    QPushButton *acceptButton_1;
+
+    // odpowiedzialny za zmiane strony
+    QStackedWidget *stackedWidget;
+
+    // pomocnicze
+    void switchToPage(int pageIndex);
+    void connectBtnHit();
+    void sendBtnHit();
+    void disconnectBtnHit();
 };
 #endif // MAINWINDOW_H
